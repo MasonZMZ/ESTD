bbf7318aa8db6db6080d03c823d45633e619f065
Mingzhe Zhang <mingzhe.zhang@uqconnect.edu.au>
2022-11-17 20:43:38 +1000

    Update train_estd.py

--- src/train_estd.py
+++ src/train_estd.py
@@ -10,9 +10,10 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from data import decode_sents, decode_sents_
 from model import create_mask, generate_square_subsequent_mask
 
+
 def train_epoch(num_epochs, Generator, discriminator, train_dataloader, 
-                test_dataloader, optimizer_D, optimizer, loss_fn, loss_fn2,
-                loss_fn_d, model_bert, Lambda_1, Lambda_2, Contorl, emp_bos_idx, 
+                test_dataloader, optimizer_D, optimizer, loss_fn, loss_fn_t,
+                loss_fn_d, model_bert, w_d, w_t, w_s, emp_bos_idx, 
                 emp_dict_rev, act_dict_rev, device, discriminative=True, theta=False):
 
     def pad_shortsent(sents, lens_long):
@@ -38,57 +39,62 @@ def train_epoch(num_epochs, Generator, discriminator, train_dataloader,
     @torch.no_grad()
     def test_translate(emp_bos_idx):
         Generator.eval()
-        for i, (src, _, tgt, _) in enumerate(test_dataloader):
+        for i, (src, _, empathy_sent, _) in enumerate(test_dataloader):
             src = src.transpose(0, 1)
-            tgt = tgt.transpose(0, 1)
-            tgt_input = tgt[:-1, :]
+            empathy_sent = empathy_sent.transpose(0, 1)
+            empathy_input = empathy_sent[:-1, :]
 
-            src_mask, _, _, _ = create_mask(src, tgt_input)
+            src_mask, _, _, _ = create_mask(src, empathy_input)
             pred = translate(src, src_mask, emp_bos_idx)
-            print(decode_sents(src.transpose(0, 1), emp_dict_rev, act_dict_rev, False))
-            print(decode_sents(tgt.transpose(0, 1), emp_dict_rev, act_dict_rev)[i])
-            print(decode_sents(pred.transpose(0, 1), emp_dict_rev, act_dict_rev)[i])
+            # print(decode_sents(tgt.transpose(0, 1), emp_dict_rev, act_dict_rev)[-1])
+            # print(decode_sents(pred.transpose(0, 1), emp_dict_rev, act_dict_rev)[-1])
             break
 
     Generator.train()
-    for epoch in range(num_epochs):
+    for epoch in tqdm(range(num_epochs)):
 
-        for i, (src, src_lens, tgt, tgt_lens) in enumerate(tqdm(train_dataloader)):
+        for i, (src, src_lens, empathy_sent, tgt_lens) in enumerate(train_dataloader):
             
-            if src.shape[1] < tgt.shape[1]:
-                src = pad_shortsent(src, tgt.shape[1])
+            if src.shape[1] < empathy_sent.shape[1]:
+                src = pad_shortsent(src, empathy_sent.shape[1])
             else:
-                tgt = pad_shortsent(tgt, src.shape[1])
+                empathy_sent = pad_shortsent(empathy_sent, src.shape[1])
 
             src = src.transpose(0, 1)
-            tgt = tgt.transpose(0, 1)
+            empathy_sent = empathy_sent.transpose(0, 1)
 
-            tgt_input = tgt[:-1, :]
-            tgt_out = tgt[1:, :]
+            empathy_input = empathy_sent[:-1, :]
+            empathy_out = empathy_sent[1:, :]
 
             src_out = src[1:, :]
 
-            src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
+            src_mask, empathy_mask, src_padding_mask, empathy_padding_mask = create_mask(src, empathy_input)
             
             optimizer.zero_grad()
-            g_fake_data = Generator(src, tgt_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
-
-            pred_sent = translate(src, src_mask, emp_bos_idx, tgt_out.shape[0] - 1)
+            generated_sent = Generator.forward(src, 
+                                               empathy_input, 
+                                               src_mask, 
+                                               empathy_mask, 
+                                               src_padding_mask, 
+                                               empathy_padding_mask, 
+                                               src_padding_mask)
+            
+            pred_sent = translate(src, src_mask, emp_bos_idx, empathy_out.shape[0] - 1)
             orig_sent = src_out
 
             pred_sent = decode_sents(pred_sent, emp_dict_rev, act_dict_rev) 
             orig_sent = decode_sents(orig_sent, emp_dict_rev, act_dict_rev, False)
             
             # ============= Discriminator =============
-
             if discriminative:
-                dg_fake_pred = discriminator.predict(pred_sent)
+                empathy_scores = discriminator.predict(pred_sent)
 
-                dg_fake_label = torch.ones(dg_fake_pred.shape[0], dtype=torch.float).to(device)
-                dg_fake_label = torch.where(dg_fake_label == 1.0, 6.0, dg_fake_label)
+                # 6.0 is the max empathy score
+                max_empathy_score = torch.ones(empathy_scores.shape[0], dtype=torch.float)
+                max_empathy_score = torch.where(max_empathy_score == 1.0, 6.0, max_empathy_score)
                 
-                dg_fake_error = loss_fn_d(dg_fake_pred.reshape(-1, 1).to(device), dg_fake_label.reshape(-1, 1).to(device))  # 6.0 is the max empathy score
-                dg_fake_error = Variable(dg_fake_error, requires_grad = True)
+                loss_d = loss_fn_d(empathy_scores.reshape(-1, 1), max_empathy_score.reshape(-1, 1)) 
+                loss_d = Variable(loss_d, requires_grad=True)
 
             pred_sent = model_bert.encode(pred_sent)
             orig_sent = model_bert.encode(orig_sent)
@@ -96,23 +102,28 @@ def train_epoch(num_epochs, Generator, discriminator, train_dataloader,
             pred_sent = torch.from_numpy(pred_sent)
             orig_sent = torch.from_numpy(orig_sent)
 
-            loss1 = loss_fn(g_fake_data.reshape(-1, g_fake_data.shape[-1]), tgt_out.reshape(-1))
+            loss = loss_fn(generated_sent.reshape(-1, generated_sent.shape[-1]), empathy_out.reshape(-1))
 
             if theta:
-                loss2 = loss_fn2(pred_sent, orig_sent)
-                loss2 = 1 - loss2
-                loss2 = Variable(loss2.mean(), requires_grad=True)
+                loss_t = loss_fn_t(pred_sent, orig_sent)
+                loss_t = Variable(loss_t.mean(), requires_grad=True)
 
-                losses = 0.7 * loss1 + 0.05 * loss2 + 0.025 * dg_fake_error 
+            if theta and discriminative:
+                losses = w_s * loss + w_t * loss_t + w_d * loss_d 
+                losses.backward() 
+                
+            elif theta and discriminative == False:
+                losses = w_s * loss + (w_t + w_d * 10) * loss_t
                 losses.backward()
-            
+                
+            elif theta == False and discriminative:
+                losses = w_s * loss + (w_t * 0.1 + w_d) * loss_d
+                losses.backward()
+                
+            elif theta == False and discriminative == False:
+                losses = loss
+                losses.backward()
+
             optimizer.step()
-
-            if i % 100 == 0:
-                test_translate(emp_bos_idx)
-                print(loss1.item())
-                print(f"Generator Training: {i + 1}/{len(train_dataloader)}")
-
-
-
-    
+            
+    return losses.item()
